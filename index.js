29c2f4a88a4a3c8b00bb5dd55d529c54fe5b87e0
sophiesmith6 <18smiths@corjesu.org>
2018-06-26 15:29:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 $(document).ready(function (){
 });
+
+function searchRepositories(){
+  const searchTerms = $("#searchTerms").val();
+  $.get(`#https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    S(`#results`).html(data);
+  })
+}
+
+var renderSearchResults = (data) => data.items.map(results => renderSearchResults(result))
+
+var renderSearchResults = (result) => 
